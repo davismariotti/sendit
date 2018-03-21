1c8e683fe894e65ef28996f27d64c661aab2c469
Davis Mariotti <davismariotti@gmail.com>
2018-03-20 23:53:40 -0700

    Add a moving background

--- SendIt/ui/GameScene.swift
+++ SendIt/ui/GameScene.swift
@@ -15,11 +15,15 @@ class GameScene: SKScene {
     var location = CGPoint.zero
     var ball: SKShapeNode = SKShapeNode(circleOfRadius: 15)
     var background: SKSpriteNode = SKSpriteNode(imageNamed: "background")
+    var background2: SKSpriteNode = SKSpriteNode(imageNamed: "background")
 
     override func didMove(to view: SKView) {
         background.size = frame.size
         background.position = CGPoint(x: view.frame.size.width / 2, y: view.frame.size.height / 2)
         addChild(background)
+        background2.size = frame.size
+        background2.position = CGPoint(x: view.frame.size.width / 2, y: view.frame.size.height * 1.5)
+        addChild(background2)
 
         let path = CGMutablePath()
         path.addArc(center: CGPoint.zero,
@@ -41,6 +45,7 @@ class GameScene: SKScene {
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
         moveNode()
+        moveBackground()
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -66,6 +71,17 @@ class GameScene: SKScene {
             let speed: CGFloat = (location.x - ball.position.x) / 20
             let newLocation = ball.position.x + speed
             ball.position = CGPoint(x: newLocation, y: ball.position.y)
+        }
+    }
+
+    func moveBackground() {
+        background.position = CGPoint(x: background.position.x, y: background.position.y - 1)
+        if background.frame.maxY == 0 {
+            background.position = CGPoint(x: background.position.x, y: view!.frame.size.height * 1.5)
+        }
+        background2.position = CGPoint(x: background2.position.x, y: background2.position.y - 1)
+        if background2.frame.maxY == 0 {
+            background2.position = CGPoint(x: background2.position.x, y: view!.frame.size.height * 1.5)
         }
     }
 }
